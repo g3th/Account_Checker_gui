9fd741d82c7621435d5c685e50093c0bbac1aa7c
Roberto <mrrobertrausch@gmail.com>
2022-09-02 05:33:54 +0100

    modified:   split_combos.py

--- split_combos.py
+++ split_combos.py
@@ -8,15 +8,17 @@ from pathlib import Path
 class split_combos():
 
 	def __init__(self, root):
+
+		self.clean_list=[]
 		self.split_combos_button = Button(root, text = 'Split Combos', font=('Arial', 12), command= partial(self.combo_splitter,'disney',root))
 		self.split_combos_button.place(x=342, y=192)
-		self.split_text_completed= Label(root, bg='goldenrod3',text='Done Splitting - Total Combos {}'.format(str(len(self.clean_list))), font=('Arial',18))
+		self.split_text_completed= Label(root, bg='goldenrod3',font=('Arial',18))
 	
 	def destroy_split_button(self):
 		self.split_combos_button.destroy()
 
 	def combo_splitter(self,service,root):
-		self.clean_list=[]	
+	
 		directory = str(Path(__file__).parent)
 		combo_list_name = service
 		with open(combo_list_name, 'r') as combo:
@@ -29,6 +31,7 @@ class split_combos():
 		os.remove(service)
 		os.rename(parsed_list_name, service)		
 		self.destroy_split_button()
+		self.split_text_completed['text']='Done Splitting - Total Combos {}'.format(str(len(self.clean_list))), 
 		self.split_text_completed.place(x=48,y=245)
 
 	def destroy_info_label(self):
