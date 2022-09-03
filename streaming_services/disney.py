71d76f49ed9e6f668475db057f46f1e8037c5a27
g3th <theinkandpen@yahoo.co.uk>
2022-09-03 02:16:03 +0100

    renamed:    disney.py -> streaming_services/disney.py 	renamed:    espn.py -> streaming_services/espn.py 	modified:   checker.py 	modified:   graphics/title.py 	modified:   split_combos.py

--- streaming_services/disney.py
+++ streaming_services/disney.py
@@ -6,6 +6,7 @@ from functools import partial
 from split_combos import split_combos
 from tkinter import *
 from tkinter import ttk
+from tkinter.ttk import *
 from bs4 import BeautifulSoup as soup
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, InvalidSessionIdException
 from selenium.webdriver.chrome.options import Options
@@ -15,6 +16,7 @@ from pathlib import Path
 class disney_checker():
 
 	def __init__(self, root):
+	
 		self.infobox = Text(root, bg='goldenrod3',font=('Arial',11),height = 360, width=460)
 		self.file_directory = str(Path(__file__).parent)+'/disney'
 		self.page = 'https://www.disneyplus.com/login'
@@ -24,32 +26,43 @@ class disney_checker():
 		self.browser_options = Options()
 		self.browser_options.add_argument = ('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36')		
 		self.browser_options.headless = True
+		self.start_checker = Button(root, text = 'Start Checker',command = partial(self.check_the_accounts,root), style='GUI_Buttons.TButton')
 		
 	def split_combo_file(self,root):
+	
 		self.split_combos = split_combos(root)
-		self.split_combos
 	
 	def draw_checker_button(self,root):
-		self.start_checker = Button(root, text = 'Start Checker', font=('Arial', 13), command = partial(self.check_the_accounts,root))
+			
 		self.start_checker.place(x=328,y=560)
 			
 	def draw_the_infobox(self):
+	
 		self.infobox.place(x=58,y=288,width=681,height=250)
 		
 	def split_username_and_password(self):
+	
 		os.makedirs('accounts',exist_ok=True)
 		with open(self.file_directory, 'r') as disney:
 			for line in disney.readlines():
 					self.users.append(line.split(':')[0].strip())
 					self.passwords.append(line.split(':')[1].strip())
-									
+						
+	def destroy_all_elements(self):
+	
+		self.split_combos.destroy_split_button()
+		self.start_checker.destroy()
+		self.infobox.destroy()
+		self.split_combos.destroy_info_label()
+		
 	def check_the_accounts(self,root):
+	
 		self.draw_the_infobox()
 		self.split_username_and_password()
 		self.start_checker.destroy()
 		self.split_combos.destroy_split_button()
 		self.split_combos.destroy_info_label()
-		checking_progress_label = Label(root,bg='goldenrod3', text = 'Opening Browser...',font=('Arial',12))
+		checking_progress_label = Label(root,background='goldenrod3', text = 'Opening Browser...',font=('Arial',12))
 		checking_progress_label.place(x=68,y=238)
 		index = 0
 		error_on_first_page = False
@@ -102,7 +115,6 @@ class disney_checker():
 							if 'Incorrect Password' in str(password_or_429_error):
 								self.infobox.insert(END," | {}:{} ---> Password is incorrect\n".format(self.users[index],self.passwords[index]))
 							if 'Due to' in str(password_or_429_error):
-								#print(password_or_429_error)
 								self.infobox.insert(END," | Login Blocked.\n")
 						if browser.find_elements_by_xpath('//*[@id="remove-main-padding_index"]/div/div/section/h2'):
 							valid = browser.find_element_by_xpath('//*[@id="remove-main-padding_index"]/div/div/section/h2').text
