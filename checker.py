de3797d3a3228d0c4b4cc101d20c77edbe0a9224
g3th <theinkandpen@yahoo.co.uk>
2022-09-07 03:11:29 +0100

    moving titles to a different module 	modified:   checker.py 	modified:   graphics/title.py

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ import threading
 from services import services_elements
 from streaming_services.disney import disney_checker
 from streaming_services.espn import espn_checker
+from graphics.title import create_app_title
 from graphics.title import main_title, disney_title, espn_title
 from tkinter import *
 from tkinter.ttk import *
@@ -20,34 +21,14 @@ class checker_gui():
 		self.gui.resizable(False, False)
 		self.gui.geometry('800x600')
 		self.gui.title('Selenium Webdriver Account Checkers')
-		self.titlebox= Canvas(self.gui,bg='skyblue2',width=510,height=178)
+		self.titlebox= create_app_title(self.gui)
 		self.servicesbox= Canvas(self.gui,bg='skyblue3',width=710,height=320)
 		self.back_button = Button(self.gui, text = 'Back', command= self.return_to_main_page, style='GUI_Buttons.TButton')
 	
-	def create_main_title(self,x1,y1,titletext,colour):
-
-		self.titlebox.create_text(x1,y1,text=titletext,fill=colour,font=('Courier New',8),tag='top_title')
-		self.titlebox.create_text(x1,y1,text=titletext,fill=colour,font=('Courier New',8),tag='bottom_title')
-
-	def colourize_main_title(self):
-
-		x1=233;y1=18;x2=290;y2=108
-		colour=['indigo', 'blueviolet', 'purple1', 'purple2', 'purple3', 'purple4', 'mediumpurple', 'mediumpurple1', 'mediumpurple2', 'mediumpurple3', 'mediumpurple4']
+	def draw_gui_titles(self):
+	
 		title=main_title()
-		self.titlebox.place(x=148,y=15)
-		for i in range(len(title[0])):
-			self.create_main_title(x1,y1,title[0][i]+'\n',colour[i])
-			self.create_main_title(x2,y2,title[1][i]+'\n',colour[i])
-			y1=y1+12
-			y2=y2+12
-		self.titlebox.config(state = DISABLED)
-	
-	def draw_services_title(self, title,x1,y1,x2,y2):
-	
-		self.titlebox.delete('top_title')
-		self.titlebox.delete('bottom_title')
-		self.titlebox.create_text(x1,y1,text=title[0],font=('Courier New',8),tag='top_title')
-		self.titlebox.create_text(x2,y2,text=title[1],font=('Courier New',8),tag='bottom_title')
+		self.titlebox.create_title(233,18,290,100,title)
 	
 	def create_back_button(self,xpos,ypos):
 
@@ -62,8 +43,8 @@ class checker_gui():
 
 		self.services = services_elements()
 		self.services.services_buttons(self.gui, 'Disney+', self.disney, 115, 315)
-		self.services.services_buttons(self.gui, 'ESPN+', self.espn_plus, 314, 315)
-		self.services.services_buttons(self.gui, 'Paramount+', self.espn_plus, 513, 315)
+		self.services.services_buttons(self.gui, 'Paramount+', self.espn_plus, 314, 315)
+		self.services.services_buttons(self.gui, 'ESPN+', self.espn_plus, 513, 315)
 		self.services.services_buttons(self.gui, 'Dazn', self.espn_plus, 115, 380)
 		self.services.services_buttons(self.gui, 'HBO', self.espn_plus, 314, 380)
 		self.services.services_buttons(self.gui, 'Fubo', self.espn_plus, 513, 380)
@@ -80,9 +61,7 @@ class checker_gui():
 		self.labels()
 		self.back_button.place_forget()
 		self.services_buttons()
-		self.titlebox.delete('top_title')
-		self.titlebox.delete('bottom_title')
-		self.colourize_main_title()
+		self.draw_gui_titles()
 			
 	def labels(self):
 		
@@ -90,23 +69,22 @@ class checker_gui():
 		self.service_label.place(x=52, y=245)
 
 	def disney(self):
-	
-		title=disney_title()
+		
+		title = disney_title()
+		self.titlebox.create_title(220,45,300,128,title)
 		self.checker = disney_checker(self.gui)
 		self.service_label.destroy()
 		self.services.services_destroy()
 		self.back_button_own_thread()
-		self.draw_services_title(title,220,45,300,128)
 		self.checker.split_combo_file(self.gui)
 		self.checker.draw_checker_button()
 
 	def espn_plus(self):
 	
-		title=espn_title()
+		title = espn_title()
 		self.checker = espn_checker(self.gui)
 		self.service_label.destroy()
 		self.services.services_destroy()
-		self.draw_services_title(title,280,40,300,125)
 		self.back_button_own_thread()
 		
 	def loop(self):
@@ -115,7 +93,7 @@ class checker_gui():
 		
 if __name__ == '__main__':
 	checker = checker_gui()
-	checker.colourize_main_title()
+	checker.draw_gui_titles()
 	checker.services_buttons()
 	checker.draw_services_box()
 	checker.labels()
--- graphics/title.py
+++ graphics/title.py
@@ -1,24 +1,42 @@
+from tkinter import *
+
+class create_app_title:
+	
+	def __init__(self, root):
+	
+		self.titlebox= Canvas(root,bg='WhiteSmoke',width=510,height=178)
+	
+	def create_title(self, x1,y1,x2,y2,header):
+	
+		self.titlebox.delete('top')
+		self.titlebox.delete('bottom')
+		title = header
+		self.titlebox.place(x=148,y=15)
+		for i in range(len(title[0])):
+			self.titlebox.create_text(x1,y1,text = title[0][i],fill = title[2][i],font=('Courier New',8),tag = 'top')
+			y1=y1+12
+		for i in range(len(title[1])):
+			self.titlebox.create_text(x2,y2,text = title[1][i],fill = title[3][i],font=('Courier New',8),tag = 'bottom')
+			y2=y2+12
+
 def main_title():
+
+	colour_top=['#000000', 'blueviolet', 'purple1', 'purple2', 'purple3', 'purple4']
+	colour_bottom=['lightskyblue','lightskyblue1','lightskyblue2','lightskyblue3','lightskyblue4','lightskyblue3','lightskyblue2']
 	account= ['    █████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███╗   ██╗████████╗','   ██╔══██╗██╔════╝██╔════╝██╔═══██╗██║   ██║████╗  ██║╚══██╔══╝','███████║██║     ██║     ██║   ██║██║   ██║██╔██╗ ██║   ██║','██╔══██║██║     ██║     ██║   ██║██║   ██║██║╚██╗██║   ██║','██║  ██║╚██████╗╚██████╗╚██████╔╝╚██████╔╝██║ ╚████║   ██║','╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝']
 	checker=['██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗███████╗██████╗','██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██╔════╝██╔══██╗','██║     ███████║█████╗  ██║     █████╔╝ █████╗  ██████╔╝','██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗','╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║',' ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝','	 			github.com/g3th']
-	return account, checker
+	return account, checker, colour_top, colour_bottom
 
 def disney_title():
-	disney='''██████╗ ██╗███████╗███╗   ██╗███████╗██╗   ██╗
-██╔══██╗██║██╔════╝████╗  ██║██╔════╝╚██╗ ██╔╝
-██║  ██║██║███████╗██╔██╗ ██║█████╗   ╚████╔╝ 
-██║  ██║██║╚════██║██║╚██╗██║██╔══╝    ╚██╔╝  
-██████╔╝██║███████║██║ ╚████║███████╗   ██║   
-╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝   ╚═╝'''
-	plus='''██████╗ ██╗     ██╗   ██╗███████╗
-██╔══██╗██║     ██║   ██║██╔════╝
-██████╔╝██║     ██║   ██║███████╗
-██╔═══╝ ██║     ██║   ██║╚════██║
-██║     ███████╗╚██████╔╝███████║
-╚═╝     ╚══════╝ ╚═════╝ ╚══════╝'''
-	return disney, plus
+
+	colour_top=['indigo', 'blueviolet', 'purple1', 'purple2', 'purple3', 'purple4']
+	colour_bottom=['lightskyblue','lightskyblue1','lightskyblue2','lightskyblue3','lightskyblue4','lightskyblue3','lightskyblue2']
+	disney=['██████╗ ██╗███████╗███╗   ██╗███████╗██╗   ██╗','██╔══██╗██║██╔════╝████╗  ██║██╔════╝╚██╗ ██╔╝','██║  ██║██║███████╗██╔██╗ ██║█████╗   ╚████╔╝ ','██║  ██║██║╚════██║██║╚██╗██║██╔══╝    ╚██╔╝  ','██████╔╝██║███████║██║ ╚████║███████╗   ██║   ','╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝   ╚═╝']
+	plus=['██████╗ ██╗     ██╗   ██╗███████╗','██╔══██╗██║     ██║   ██║██╔════╝','██████╔╝██║     ██║   ██║███████╗','██╔═══╝ ██║     ██║   ██║╚════██║','██║     ███████╗╚██████╔╝███████║','╚═╝     ╚══════╝ ╚═════╝ ╚══════╝']
+	return disney, plus, colour_top, colour_bottom
 
 def espn_title():
+
 	espn='''███████╗███████╗██████╗ ███╗   ██╗
 ██╔════╝██╔════╝██╔══██╗████╗  ██║
 █████╗  ███████╗██████╔╝██╔██╗ ██║
@@ -32,4 +50,4 @@ def espn_title():
 ██║     ███████╗╚██████╔╝███████║
 ╚═╝     ╚══════╝ ╚═════╝ ╚══════╝'''
 	return espn, plus
-
+	
